45110c391cee008d60eb52586b459be5b3b57722
beiyate <beiyate@sina.com>
2018-05-20 00:54:40 +0800

    0520

--- Nodejs.js
+++ Nodejs.js
@@ -1,6 +1,17 @@
 'use strict'
 
-var name="node.js";
-var s=`hello,${name}`;
+/*var name="node.js";
+var s="hello";
 
-console.log(s);
+function greet(name){
+    console.log(s+ ', ' + name + '!');
+}
+module.exports=greet;*/
+
+///fs
+
+/*Node.js内置的fs模块就是文件系统模块，负责读写文件。
+
+和所有其它JavaScript模块不同的是，fs模块同时提供了异步和同步的方法。*/
+
+
---
+++ main.js
@@ -0,0 +1,8 @@
+'use strict'
+
+//import
+var greet1 =require('./Nodejs');
+
+var s='Michael';
+
+greet1(s);
